6bb382f5ab4e93f7d5468d33747720a8bd0d29a9
jinbb5531 <879504065@qq.com>
2019-07-14 23:48:31 +0800

    1.代码提交

--- erp-web/src/main/java/com/person/erp/analyze/controller/AnalyzeController.java
+++ erp-web/src/main/java/com/person/erp/analyze/controller/AnalyzeController.java
@@ -1,24 +1,22 @@
 package com.person.erp.analyze.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.itexplore.core.disk.common.utils.DateUtils;
+import com.mysql.cj.xdevapi.JsonArray;
 import com.person.erp.analyze.service.IAnlayzeService;
 import com.person.erp.common.utils.TokenUtils;
-import com.person.erp.order.dao.IOrderDAO;
 import com.person.erp.order.entity.Order;
 import com.person.erp.order.entity.OrderOperate;
-import org.springframework.beans.factory.annotation.Autowired;
+import net.sf.ehcache.search.expression.Or;
+import netscape.javascript.JSObject;
+import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 报表分析模块
@@ -33,38 +31,47 @@ public class AnalyzeController {
      * 初始化标准产量
      */
     @GetMapping("/init")
-    public Map<String, Object> initEchars(Long startDate, Long endDate){
-        HashMap res = new HashMap();
+    public JSONArray initEchars(Long startDate, Long endDate){
+        JSONArray res = new JSONArray();
         if(startDate == null){
             startDate = new Date().getTime()/1000 - 7 * 24 * 3600;
+        }else{
+            startDate = startDate/1000;
         }
         if (endDate == null){
             endDate = new Date().getTime()/1000;
+        }else{
+            endDate = endDate/1000;
         }
-        List<Order> planNumber = anlayzeService.getPlanYield(startDate, endDate, 1);
-//        List<Order> planNumber = anlayzeService.getPlanYield(startDate, endDate, TokenUtils.getUser().getSystemTag());
+        List<Order> planNumber = anlayzeService.getPlanYield(startDate, endDate, TokenUtils.getUser().getSystemTag());
         if (planNumber != null && planNumber.size() > 0){
             for (Order order : planNumber) {
-                res.put(order.getCutAt().toString(),order.getNumber());
+                JSONObject json = new JSONObject();
+                json.put(order.getCutAt().toString(),order.getNumber());
+                res.add(json);
             }
         }
         return res;
     }
     @GetMapping("/yield")
-    public Map<String, Object> raalYield(Long startDate, Long endDate){
-        HashMap res = new HashMap();
+    public JSONArray raalYield(Long startDate, Long endDate){
+        JSONArray res = new JSONArray();
         if(startDate == null){
             startDate = new Date().getTime()/1000 - 7 * 24 * 3600;
+        }else {
+            startDate = startDate/1000;
         }
         if (endDate == null){
             endDate = new Date().getTime()/1000;
+        }else {
+            endDate = endDate/1000;
         }
-        long sys = 1;
-        List<OrderOperate> planNumber = anlayzeService.getRealYield(startDate, endDate, 1);
-//        List<OrderOperate> planNumber = anlayzeService.getRealYield(startDate, endDate, TokenUtils.getUser().getSystemTag());
+        List<OrderOperate> planNumber = anlayzeService.getRealYield(startDate, endDate, TokenUtils.getUser().getSystemTag());
         if (planNumber != null && planNumber.size() > 0){
             for (OrderOperate order : planNumber) {
-                res.put(order.getOperaTime().toString(),order.getCutNum());
+                JSONObject json = new JSONObject();
+                json.put(order.getOperaTime().toString(),order.getCutNum());
+                res.add(json);
             }
         }
         return res;
